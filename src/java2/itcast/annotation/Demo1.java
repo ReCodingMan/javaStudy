5431cb2c8d972a50c1bb42b9a029be8cb4ad6444
chengcheng1021 <chengcheng1021@aliyun.com>
2020-05-08 11:40:36 +0800

    学习注解案例

---
+++ src/java2/itcast/annotation/Demo1.java
@@ -0,0 +1,7 @@
+package java2.itcast.annotation;
+
+public class Demo1 {
+    public void show(){
+        System.out.println("Demo1...show...");
+    }
+}
---
+++ src/java2/itcast/annotation/Demo2.java
@@ -0,0 +1,8 @@
+package java2.itcast.annotation;
+
+
+public class Demo2 {
+    public void show2(){
+        System.out.println("Demo2...show...");
+    }
+}
