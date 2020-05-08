90de84524022b88d9b84be73560f08e60ac1445d
chengcheng1021 <chengcheng1021@aliyun.com>
2020-05-08 11:07:27 +0800

    利用反射调用任意类任意方法

---
+++ src/java2/itcast/domain/Person.java
@@ -0,0 +1,46 @@
+package java2.itcast.domain;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public void eat(){
+        System.out.println("eat...");
+    }
+
+    public void eat(String food){
+        System.out.println("eat..." + food);
+    }
+}
---
+++ src/java2/itcast/domain/Student.java
@@ -0,0 +1,8 @@
+package java2.itcast.domain;
+
+public class Student {
+
+    public void sleep(){
+        System.out.println("sleep...");
+    }
+}
