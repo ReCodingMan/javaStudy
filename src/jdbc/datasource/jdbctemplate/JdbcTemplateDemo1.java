c303b8c5fef23cc2e7ce84c7c8b74084fea65b50
chengcheng1021 <chengcheng1021@aliyun.com>
2020-05-10 15:14:12 +0800

    jdbc template连接

---
+++ src/jdbc/datasource/jdbctemplate/JdbcTemplateDemo1.java
@@ -0,0 +1,21 @@
+package jdbc.datasource.jdbctemplate;
+
+import jdbc.datasource.utils.JDBCUtils;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ *  JdbcTemplate入门
+ */
+public class JdbcTemplateDemo1 {
+
+    public static void main(String[] args) {
+        //1、导入jar包
+        //2、创建JDBCTemplate对象
+        JdbcTemplate template = new JdbcTemplate(JDBCUtils.getDataSource());
+        //3、调用方法
+        String sql = "update student set name = 'hahah' where id = ?";
+        int count = template.update(sql, 3);
+
+        System.out.println(count);
+    }
+}
