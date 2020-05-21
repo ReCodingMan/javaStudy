321b3136ee1a9b24e7a584c72a9d16caae0e4c1b
chengcheng1021 <chengcheng1021@aliyun.com>
2020-05-21 20:07:52 +0800

    斗地主案例上传

---
+++ src/cc1021/doudizhu/Demo.java
@@ -0,0 +1,16 @@
+package cc1021.doudizhu;
+
+/**
+ *  斗地主综合案例：有序版本
+ *  1、准备牌
+ *  2、洗牌
+ *  3、发牌
+ *  4、排序
+ *  5、看牌
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+
+    }
+}
