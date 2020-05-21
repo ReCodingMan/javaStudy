a960b4e1e5f85771e024fe73355433b2b9246e62
chengcheng1021 <chengcheng1021@aliyun.com>
2020-05-21 20:21:03 +0800

    斗地主完成洗牌

--- src/cc1021/doudizhu/Demo.java
+++ src/cc1021/doudizhu/Demo.java
@@ -1,6 +1,7 @@
 package cc1021.doudizhu;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -24,6 +25,33 @@ public class Demo {
         List<String> colors = List.of("♠️", "♥️", "♣️", "♦️");
         List<String> numbers = List.of("2", "A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3");
 
+        //把大王和小王存储到集合中
+        //定义一个牌的索引
+        int index = 0;
+        poker.put(index, "大王");
+        pokerIndex.add(index);
+        index++;
+        poker.put(index, "小王");
+        pokerIndex.add(index);
+        index++;
 
+        //循环嵌套遍历两个集合，组装52张牌，存储到集合中
+        for (String number : numbers){
+            for (String color : colors){
+                poker.put(index, color + number);
+                pokerIndex.add(index);
+                index++;
+            }
+        }
+
+        //System.out.println(poker);
+        //System.out.println(pokerIndex);
+
+        /**
+         *  2、洗牌
+         *  使用 Collections 中的方法 shuffle(List)
+         */
+        Collections.shuffle(pokerIndex);
+        //System.out.println(pokerIndex);
     }
 }
