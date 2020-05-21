56b00352b381afeedd074d070cc665d4d2133747
chengcheng1021 <chengcheng1021@aliyun.com>
2020-05-21 20:37:06 +0800

    完成排序

--- src/cc1021/doudizhu/Demo.java
+++ src/cc1021/doudizhu/Demo.java
@@ -53,5 +53,44 @@ public class Demo {
          */
         Collections.shuffle(pokerIndex);
         //System.out.println(pokerIndex);
+
+        /**
+         * 3、发牌
+         * 定义4个集合，存储玩家牌的索引，和底牌索引
+         */
+        ArrayList<Integer> player01 = new ArrayList<>();
+        ArrayList<Integer> player02 = new ArrayList<>();
+        ArrayList<Integer> player03 = new ArrayList<>();
+        ArrayList<Integer> dipai = new ArrayList<>();
+        //遍历存储牌索引的 List 集合，获取每一个牌的索引
+        for (int i=0; i<pokerIndex.size(); i++){
+            Integer in = pokerIndex.get(i);
+            //先判断底牌
+            if(i>=51){
+                //给底牌发牌
+                dipai.add(in);
+            }else if (i%3 == 0){
+                //给玩家1发牌
+                player01.add(in);
+            }else if (i%3 == 1){
+                //给玩家2发牌
+                player02.add(in);
+            }else if (i%3 == 2){
+                //给玩家3发牌
+                player03.add(in);
+            }
+        }
+
+        /**
+         *  4、排序
+         *  使用 Collections 中的方法 sort(List)
+         */
+        Collections.sort(player01);
+        Collections.sort(player02);
+        Collections.sort(player03);
+        Collections.sort(dipai);
+
+
     }
+
 }
