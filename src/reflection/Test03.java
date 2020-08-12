561f4c56a64382dadafd8558b718a6cee6e1bc15
chengcheng1021 <chengcheng1021@aliyun.com>
2020-08-12 15:10:57 +0800

    类加载内存

---
+++ src/reflection/Test03.java
@@ -0,0 +1,21 @@
+package reflection;
+
+public class Test03 {
+    public static void main(String[] args) {
+        A a = new A();
+        System.out.println(A.m);
+    }
+}
+
+class A {
+    static {
+        System.out.println("A类静态代码块初始化");
+        m = 300;
+    }
+
+    static int m = 100;
+
+    public A() {
+        System.out.println("A类无参构造初始化");
+    }
+}
