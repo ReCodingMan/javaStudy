2f055d2c1ce433007693f90645443e7db658692f
chengcheng1021 <chengcheng1021@aliyun.com>
2020-08-12 17:13:49 +0800

    获取泛型类型

---
+++ src/reflection/Test07.java
@@ -0,0 +1,48 @@
+package reflection;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Map;
+
+//通过反射获取泛型
+public class Test07 {
+
+    public void test01(Map<String, User> map, List<User> list) {
+        System.out.println("test01");
+    }
+
+    public Map<String, User> test02() {
+        System.out.println("test02");
+        return null;
+    }
+
+    public static void main(String[] args) throws NoSuchMethodException {
+        Method method = Test07.class.getMethod("test01", Map.class, List.class);
+
+        Type[] genericParameterTypes = method.getGenericParameterTypes();
+        for (Type genericParameterType : genericParameterTypes) {
+            System.out.println("#" + genericParameterType);
+            //如果参数是复合型类型
+            if (genericParameterType instanceof ParameterizedType) {
+                Type[] actualTypeArguments = ((ParameterizedType) genericParameterType).getActualTypeArguments();
+                for (Type actualTypeArgument : actualTypeArguments) {
+                    System.out.println(actualTypeArgument);
+                }
+            }
+        }
+
+        System.out.println("---------------------");
+        Method method2 = Test07.class.getMethod("test02", null);
+
+        Type genericReturnType = method2.getGenericReturnType();
+        if (genericReturnType instanceof ParameterizedType) {
+            Type[] actualTypeArguments = ((ParameterizedType) genericReturnType).getActualTypeArguments();
+            for (Type actualTypeArgument : actualTypeArguments) {
+                System.out.println(actualTypeArgument);
+            }
+        }
+
+    }
+}
